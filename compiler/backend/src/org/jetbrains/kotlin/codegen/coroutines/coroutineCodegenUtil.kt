9048635a7bc8c726925ef06db2ecf2ee2c6d5720
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2018-03-13 12:16:13 +0300

    Change tests

--- compiler/backend/src/org/jetbrains/kotlin/codegen/coroutines/coroutineCodegenUtil.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/coroutines/coroutineCodegenUtil.kt
@@ -361,7 +361,7 @@ fun createMethodNodeForCoroutineContext(
     functionDescriptor: FunctionDescriptor,
     languageVersionSettings: LanguageVersionSettings
 ): MethodNode {
-    assert(functionDescriptor.isBuiltInCoroutineContext(languageVersionSettings)) {
+    assert(functionDescriptor.isBuiltInCoroutineContext()) {
         "functionDescriptor must be kotlin.coroutines.intrinsics.coroutineContext property getter"
     }
 
